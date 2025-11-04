2daf11929aab76f56b9f90658f32b70670230159
Muesli1 <mc01@neumann.to>
2025-11-04 22:12:39 +0100

    feat: vLLM test setup

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # PL-MAI_VPS
+
+## Setup
+
+Recommend & tested python version is 12.x
+
+!! Running in a virtual environment is highly recommended !!
+
+Install vLLM to run this repo, follow the guide for either:
+
+- [GPU](https://docs.vllm.ai/en/latest/getting_started/installation/gpu.html) (CUDA, AMD ROCm or Intel XPU)
+- or [CPU](https://docs.vllm.ai/en/latest/getting_started/installation/cpu.html#intelamd-x86) (Intel/AMD x86, ARM
+  AArch64, Apple silicon, IBM Z S390X)
+
+## Full setup for Linux and CUDA 12.8 GPU
+
+Using [pyenv](https://docs.vllm.ai/en/latest/getting_started/installation/cpu.html#intelamd-x86) for version management,
+pip for package management (vLLM recommends uv), and venv for the virtual environment
+
+```bash
+python -m venv .venv 
+source .venv/bin/activate
+pip install --upgrade pip
+
+pip install vllm --extra-index-url https://download.pytorch.org/whl/cu128
+```
+
+## vLLM test
+
+Running `python src/pl_mai_vps/setup/vllm_test.p` runs the Qwen2.5-VL-3B-Instruct model and starts a console chat
+dialog.
+
+It reserves realistic memory necessary for 50 image frames out of 480p videos and inputs two images of unicorns via
+URLs.
+
+If the setup is working, the model can correctly answer questions about the images and answer general questions.
+
+Note: The model (7.1 GB) will be downloaded and stored in `HF_HOME` (or on Linux default `~/.cache/huggingface/hub`).
+This can take some time, depending on download speed.
+
---
+++ src/pl_mai_vps/setup/vllm_test.py
@@ -0,0 +1,77 @@
+from vllm import LLM
+
+# 50 needs about 14 GB of VRAM
+frame_amount = 50
+# Approximately N images of size + 100 tokens output
+# 480p = 854x480 pixels
+# 28x28 pixels ~= 1 token => 480p ~= 31 x 18 tokens ~= 558 tokens
+max_model_len = 558 * frame_amount + 100
+
+print(
+    f"Trying to load Qwen2.5-VL-3B-Instruct model with space for {frame_amount}x 480p frames and 100 output tokens...")
+
+# Initialize the vLLM engine.
+llm = LLM(
+    model="Qwen/Qwen2.5-VL-3B-Instruct",
+    max_model_len=max_model_len,
+    # No prefix caching, will also not be used in experiments
+    # Only make sense for chat conversations or static prompts
+    enable_prefix_caching=False,
+    limit_mm_per_prompt={"image": frame_amount}
+)
+
+conversation = [
+    {
+        "role": "system",
+        "content": "You are a helpful assistant",
+    },
+    {
+        "role": "user",
+        "content": [{
+            "type": "image_url",
+            "image_url": {
+                "url": "https://upload.wikimedia.org/wikipedia/commons/7/70/Oftheunicorn.jpg"
+            }
+        }, {
+            "type": "image_url",
+            "image_url": {
+                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Domenichounicorndetail.jpg/1024px-Domenichounicorndetail.jpg"
+            }
+        }]
+    }
+]
+
+print("=" * 20)
+print("Type message and press enter to chat with model ('exit' for quitting the app)")
+print("There are currently two images unicorns in the context")
+print("=" * 20)
+
+while True:
+    line = input()
+
+    if line.lower() == "exit":
+        break
+
+    conversation.append(
+        {
+            "role": "user",
+            "content": line,
+        }
+    )
+
+    sampling_params = llm.get_default_sampling_params()
+    sampling_params.max_tokens = 500
+    outputs = llm.chat(conversation, sampling_params)
+
+    assert len(outputs) == 1
+    output = outputs[0]
+
+    generated_text = output.outputs[0].text
+    # print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+    print("=" * 5)
+    print(generated_text)
+
+    conversation.append({
+        "role": "assistant",
+        "content": generated_text,
+    })
